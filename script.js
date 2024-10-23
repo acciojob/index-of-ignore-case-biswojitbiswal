0e093cbbd10aaddbc13b5282330d91bee5ee98e7
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-23 11:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	const index = s1.toLowerCase().indexOf(s2.toLowerCase());
+	return index;
 }
 
 // Please do not change the code below
